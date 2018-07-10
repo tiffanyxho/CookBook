6b06d4273e40824bdd73072f86edd2fc2984e37e
TiffanyH <tiffanyxho@gmail.com>
2018-07-10 15:49:07 -0700

    home button always shows in navbar now even when simple recipes is clicked

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ document.getElementById("navbar-simple-recipes").addEventListener("click", funct
 
     document.getElementById("recipes").style.display = "initial";
     document.getElementsByClassName("navbar-default")[0].style.display = "none";
+    //document.getElementById("navbar-default").style.display = "none";
     document.getElementById("navbar-recipes").style.display = "initial";
     document.getElementById("recipes").getElementsByTagName("h2")[0].style.display = "initial";
 });
